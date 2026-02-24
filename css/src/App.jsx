686dcb7a9f89faa66e027110f9464f0c43e6c380
KunalxKushwaha <kushwahakunal733@gmail.com>
2026-02-25 00:35:25 +0530

    css revised

---
+++ css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/Button'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
---
+++ css/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div className='button'>
+        Click Me!!
+      
+    </div>
+  )
+}
+
+export default Button
